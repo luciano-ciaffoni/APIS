b8aec3ce033c5689c3f75fffe3478febee7c7051
alumno <alumno@Instituto.local>
2025-08-20 08:19:53 -0300

    20/8/2025

--- lucho.js
+++ lucho.js
@@ -1,5 +1,5 @@
 
-let titulo = document.querySelector("#titulo")
+/*let titulo = document.querySelector("#titulo")
 let imagen = document.querySelector("#imagen")
 let boton = document.querySelector("#boton")
 
@@ -10,5 +10,45 @@ boton.onclick = function (){
             console.log(datos)
             titulo.textContent = datos.name
             imagen.src = datos.sprite.front_shiny
+        })
+}
+ 
+
+
+let titulo = document.querySelector("#titulo")
+let imagen = document.querySelector("#imagen")
+let boton = document.querySelector("#boton")
+
+
+boton.onclick = function () {
+    fetch("https://pokeapi.co/api/v2/pokemon-form/89/")
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos)
+            titulo.textContent = datos.name
+            imagen.src = datos.sprites.front_default 
+        })
+}
+
+*/
+
+let titulo = document.querySelector("#titulo")
+let imagen = document.querySelector("#imagen")
+let boton = document.querySelector("#boton")
+
+boton.onclick = function () {
+    fetch("https://pokeapi.co/api/v2/pokemon/89/")
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            console.log(datos)
+            
+            // Obtener tipos
+            let tipos = datos.types.map(tipo => tipo.type.name).join(', ')
+            
+            // Mostrar nombre + tipo
+            titulo.textContent = `#${datos.id} - ${datos.name} (Tipo: ${tipos})`
+            
+            // Imagen
+            imagen.src = datos.sprites.front_default
         })
 }
